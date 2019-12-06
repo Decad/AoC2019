05c4f7452f5dc058c322f8002f54ee2aae3b87fd
DCook <declancook44@gmail.com>
2019-12-06 00:08:04 +0000

    Implement step 2 and refactor

--- 5/AoC5/IntcodeComputer.cs
+++ 5/AoC5/IntcodeComputer.cs
@@ -9,18 +9,30 @@ namespace AoC5
         public int[] mem;
         private int pc = 0;
         private Stack<bool> modes = new Stack<bool>();
+        private Dictionary<int, Action> opcodes;
+        private bool halted = false;
 
         public IntcodeComputer(int[] mem)
         {
             this.mem = mem;
+            this.opcodes = new Dictionary<int, Action>() {
+                { 1, Add },
+                { 2, Mult },
+                { 3, Input },
+                { 4, Output },
+                { 5, JmpTrue },
+                { 6, JmpFalse },
+                { 7, LessThan },
+                { 8, Equal },
+                { 99, () => halted = true },
+            };
         }
 
-        internal int[] Run()
+        public void Run()
         {
-            bool running = true;
-            while (running)
+            while (!halted)
             {
-                int instruction = mem[pc];
+                int instruction = mem[pc++];
                 int opcode = instruction % 100;
 
                 var m = (instruction / 100).ToString()
@@ -30,63 +42,90 @@ namespace AoC5
 
                 modes = new Stack<bool>(m);
 
-                pc++;
-
-                int p1 = 0;
-                int p2 = 0;
-                switch (opcode)
-                {
-                    case 1:
-                        p1 = Read();
-                        p2 = Read();
-                        Store(mem[pc], checked(p1 + p2));
-                        pc++;
-                        break;
-                    case 2:
-                        p1 = Read();
-                        p2 = Read();
-                        Store(mem[pc], checked(p1 * p2));
-                        pc++;
-                        break;
-                    case 3:
-                        Console.Write("Input:");
-                        var input = int.Parse(Console.ReadLine());
-                        modes.Push(true);
-                        Store(Read(), input);
-                        break;
-                    case 4:
-                        Console.WriteLine($"Ouput: {Read()}");
-                        break;
-                    case 99:
-                        running = false;
-                        break;
-                }
-
+                opcodes[opcode]();
             }
-
-            return mem;
         }
 
-        internal int Read()
-        {
-            var output = modes.Pop() ? mem[pc] : mem[mem[pc]];
-            pc++;
-            return output;
-        }
-
-        internal void Store(int index, int value)
-        {
-            mem[index] = value;
-        }
-
-        internal string Dump()
+        public string Dump()
         {
             return string.Join(',', mem.Select(x => x.ToString()));
         }
 
-        internal int Dump(int index)
+        public int Dump(int index)
         {
             return mem[index];
+        }
+
+        private void Add()
+        {
+            var p1 = Read();
+            var p2 = Read();
+            Store(Read(true), checked(p1 + p2));
+        }
+
+        private void Mult()
+        {
+            var p1 = Read();
+            var p2 = Read();
+            Store(Read(true), checked(p1 * p2));
+        }
+
+        private void Input()
+        {
+            Console.Write("Input:");
+            var input = int.Parse(Console.ReadLine());
+            Store(Read(true), input);
+        }
+
+        private void Output()
+        {
+            Console.WriteLine($"Ouput: {Read()}");
+        }
+
+        private void JmpTrue()
+        {
+            var p1 = Read();
+            var p2 = Read();
+            if (p1 != 0)
+            {
+                pc = p2;
+            }
+        }
+
+        private void JmpFalse()
+        {
+            var p1 = Read();
+            var p2 = Read();
+            if (p1 == 0)
+            {
+                pc = p2;
+            }
+        }
+
+        private void LessThan()
+        {
+            var p1 = Read();
+            var p2 = Read();
+            Store(Read(true), p1 < p2 ? 1 : 0);
+        }
+
+        private void Equal()
+        {
+            var p1 = Read();
+            var p2 = Read();
+            Store(Read(true), p1 == p2 ? 1 : 0);
+        }
+
+        private void Store(int index, int value)
+        {
+            mem[index] = value;
+        }
+
+        private int Read(bool i = false)
+        {
+            var output = modes.Pop() || i ? mem[pc] : mem[mem[pc]];
+            pc++;
+            return output;
         }
 
     }
